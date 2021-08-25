abb89d1acd71d351d3477d8d84d53b9f2d140a70
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2021-08-25 13:20:54 +0200

    [#4 - AndroidApp] Manage Microphone Sensor

--- App/app/src/main/java/com/example/crowdsensingapp/MapsActivity.java
+++ App/app/src/main/java/com/example/crowdsensingapp/MapsActivity.java
@@ -1,17 +1,22 @@
 package com.example.crowdsensingapp;
 
+import static java.lang.Math.log10;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
+import java.lang.*;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -27,15 +32,36 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.crowdsensingapp.databinding.ActivityMapsBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private Location actuaLocation = null;
+    private MediaRecorder  mRecorder;
+    private Button recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //binding the map fragment
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_maps);
+        recordButton = (Button) findViewById(R.id.record_btn);
+
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double powerDb = 10 * log10(getAmplitude());
+                System.out.println(powerDb + " My actual DB recorded");
+
+            }
+        });
+
+
 
 //permission request if the permissions are denied
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -67,7 +93,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             //setting the location request
             LocationRequest mLocationRequest = LocationRequest.create();
-            mLocationRequest.setInterval(1000);
+            mLocationRequest.setInterval(10000);
             mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
 
@@ -88,6 +114,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    1);
+        }else{
+           mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mRecorder.start();
+
+
+        }
 
 
 
@@ -97,14 +143,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-//binding the map fragment
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+//get the db amplitude when mrecorder is on
+    public double getAmplitude(){
+        if (mRecorder != null)
+            return  (mRecorder.getMaxAmplitude());
+        else
+            return 0;
+
     }
 
     /**
