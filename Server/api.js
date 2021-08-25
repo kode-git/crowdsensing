f9870bff01396c7cc65aa5718a1e601397da36bf
kode-git <mariosessa63@gmail.com>
2021-08-25 11:43:32 +0200

    [#3 - Server] Adding Location Table

--- Server/api.js
+++ Server/api.js
@@ -1,11 +1,8 @@
-// module of the app.js with every kinds of exportable REST Api
-
-
-// This is a dummy object
-// meanwhile we don't have a real json
-// object to send/receive
+// module of the app.js with every kinds of exportable REST Apis
 
 const Pool = require('pg').Pool
+
+// connection at the init
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -13,9 +10,74 @@ const pool = new Pool({
     password: 'blockchain',
     port: 5432,
 })
+
+// --- Locations API ---
+const getLocations = (request, response) => {
+    pool.query('select * from loc_detections', (error, results) => {
+        if(error){
+            // not happen
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getLocationById = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('select * from loc_detections where id=$1', [id], (error, results) => {
+        if(error) {
+            // location not found => 404 page redirect
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createLocation = (request, response) => {
+    const { user_id, avg_noise, cur_noise, coordinates } = request.body
+
+    pool.query('INSERT INTO loc_detections (user_id, avg_noise, cur_noise, coordinates) VALUES ($1, $2, $3, $4)', [user_id, avg_noise, cur_noise, coordinates], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Location added with ID: ${results.insertId}`)
+    })
+}
+
+const updateLocation = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { avg_noise, cur_noise, coordinates } = request.body
+    // TODO: Define how to pass coordinates
+    pool.query(
+        'UPDATE loc_detections SET avg_noise = $1, cur_noise = $2, coordinates = $3 WHERE id = $4',
+        [avg_noise, cur_noise, coordinates, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`Location modified with ID: ${id}`)
+        }
+    )
+}
+
+
+const deleteLocation = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM loc_detections WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Location deleted with ID: ${id}`)
+    })
+}
+
+
+// --- Users API ---
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) {
+            // user not found => 404 page redirect
             throw error
         }
         response.status(200).json(results.rows)
@@ -40,7 +102,7 @@ const createUser = (request, response) => {
         if (error) {
             throw error
         }
-        response.status(201).send(`User added with ID: ${result.insertId}`)
+        response.status(201).send(`User added with ID: ${results.insertId}`)
     })
 }
 
@@ -65,8 +127,6 @@ const deleteUser = (request, response) => {
 
     pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
         if (error) {
-            // undefined table or database offline
-            // TODO: Manage this error with a rendering on a 500 error status page
             throw error
         }
         response.status(200).send(`User deleted with ID: ${id}`)
@@ -74,6 +134,11 @@ const deleteUser = (request, response) => {
 }
 
 module.exports = {
+    getLocations,
+    getLocationById,
+    createLocation,
+    updateLocation,
+    deleteLocation,
     getUsers,
     getUserById,
     createUser,
--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const api = require('./api')
+const path = require('path');
 const port = 3000
+
+
 
 app.use(bodyParser.json())
 app.use(
@@ -11,15 +14,24 @@ app.use(
     })
 )
 
+
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+    response.sendFile(path.resolve(__dirname+"/../Dashboard/index.html"));
 })
 
-app.get('/users', api.getUsers)
-app.get('/users/:id', api.getUserById)
-app.post('/users', api.createUser)
-app.put('/users/:id', api.updateUser)
-app.delete('/users/:id', api.deleteUser)
+// Locations API
+app.get('/getLocations',api.getLocations )
+app.get('/getLocation/:id',api.getLocationById )
+app.post('/createLocation', api.createLocation)
+app.put('/updateLocation/:id', api.updateLocation)
+app.delete('/deleteLocation/:id', api.deleteLocation)
+
+// Users API
+app.get('/getUsers', api.getUsers)
+app.get('/getUser/:id', api.getUserById)
+app.post('/createUser', api.createUser)
+app.put('/updateUser/:id', api.updateUser)
+app.delete('/deleteUser/:id', api.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
