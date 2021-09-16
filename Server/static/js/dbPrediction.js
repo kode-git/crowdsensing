ec5923c522174258de00c4ffd87cf1e579a859b5
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2021-09-16 19:01:03 +0200

    [#33 - Server] Dump merging

--- Server/static/js/dbPrediction.js
+++ Server/static/js/dbPrediction.js
@@ -27,31 +27,6 @@ function predictionDb(dataPoint){
         
         
         });
-    
-     pointLng = dataPoint[0]
-     pointLat = dataPoint[1]
-     const spawn = require("child_process").spawn;
-    const pythonProcess = spawn('python',["static/js/predictor.py", pointLng,pointLat]);
- 
-
-    pythonProcess.stdout.on('data', (data) => {
-        console.log("AAA")
-        console.log(data.toString())
-        return data.toString()
-    });
-
-
-    pythonProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      pythonProcess.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
-
-
-
-
 
 
 }
