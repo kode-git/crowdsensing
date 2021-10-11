d1f3367f8e704f03f2704e6d9679e73a3b0ed399
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2021-10-11 20:05:12 +0200

    [#52 - Server] Scaling clustering decibels

--- Server/static/py/clustering.py
+++ Server/static/py/clustering.py
@@ -5,7 +5,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.cluster import KMeans
 import numpy as np
 import json
-
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import preprocessing
 # Create a database connection
 engine = db.create_engine(
     "postgresql://postgres:blockchain@localhost:5432/csdb")
@@ -19,7 +20,26 @@ k = int(sys.argv[1])
 input_data = gpd.GeoDataFrame()
 input_data['long'] = data['coordinates'].y
 input_data['lat'] = data['coordinates'].x
-input_data['db'] = data['db']/1000
+
+
+myMaxLong=data['coordinates'].y.max(axis=0)
+myMinLong=data['coordinates'].y.min(axis=0)
+myRangeLong=myMaxLong-myMinLong
+myMaxLat=data['coordinates'].x.max(axis=0)
+myMinLat=data['coordinates'].x.min(axis=0)
+myRangeLat=myMaxLat-myMinLat
+#setting the range for the normalization on the db value
+myRange=myRangeLong
+if(myRangeLong<myRangeLat):
+     myRange=myRangeLat
+
+
+scaler = MinMaxScaler(feature_range=(0, myRange))
+
+#scaling the db in the max range of lat and lng
+input_data['db']= preprocessing.minmax_scale(data['db'],feature_range=(0, myRange))
+
+
 kmeans = KMeans(n_clusters=k, random_state=520, n_init=40, max_iter=300).fit(input_data)
 arr = np.array(kmeans.predict(input_data))
 clusters = []
