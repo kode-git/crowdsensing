56da0348aa86890c5245114645d7eb9fbc5f55ce
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2021-10-12 12:44:31 +0200

    [#54 - Server] Update predictor without normalizer

--- Server/static/py/predictor.py
+++ Server/static/py/predictor.py
@@ -22,11 +22,10 @@ sql = "select id,coordinates,db from loc_ref_points as l1 where ST_Distance(ST_S
 
 
 data = gpd.read_postgis(sql=sql, con=con, geom_col="coordinates")
-
 flag=False
 
 
-
+#requering if the number of points are less than ten
 if(len(data)<10):
     logger.error("less than ten points")
     #if the flag is false there are less than ten point for the query result
@@ -35,46 +34,31 @@ if(len(data)<10):
     data = gpd.read_postgis(sql=sql, con=con, geom_col="coordinates")
 logger.error(len(data))
 
-train_data = gpd.GeoDataFrame()
 
+#creating the pandas dataframe
+train_data = gpd.GeoDataFrame()
 train_data['lat'] = data["coordinates"].x
 train_data['lng'] = data["coordinates"].y
-myMinLat=train_data['lat'].min()
-myMaxLat=train_data['lat'].max()
-myMinLng=train_data['lng'].min()
-myMaxLng=train_data['lng'].max()
-
-def min_max_scaling(df):
-    # copy the dataframe
-    df_norm = df.copy()
-    # apply min-max scaling
-    for column in df_norm.columns:
-        df_norm[column] = (df_norm[column] - df_norm[column].min()) / (df_norm[column].max() - df_norm[column].min())
-        
-    return df_norm
 
 
-if flag:
-    train_dataMLP= min_max_scaling(train_data)
-myLat=(float(sys.argv[2]) - myMinLat) / (myMaxLat - myMinLat)
-myLng=(float(sys.argv[1]) - myMinLng) / (myMaxLng - myMinLng)
+
 
 train_result = data['db']
 
 #training phase, when we have more than 10 elements near ourpoint, we use the knn regressor
 if flag:
-    regB = MLPRegressor(random_state=1, max_iter=5000).fit(train_dataMLP, train_result)
+    regB = MLPRegressor(random_state=1, max_iter=5000).fit(train_data, train_result)
 else:
     knn = neighbors.KNeighborsRegressor(3)
     knn.fit(train_data, train_result)
 
 
 
-#Predict
+#Predict and setting up a JSON for the client 
 if not(flag):
     outLine='\"db\":{}'.format(knn.predict([[float(sys.argv[2]),float(sys.argv[1])]])[0] )
 else:
-    outLine='\"db\":{}'.format(regB.predict([[myLat,myLng]])[0] )
+    outLine='\"db\":{}'.format(regB.predict([[float(sys.argv[2]),float(sys.argv[1])]])[0] )
 
 outLine="{" + outLine + "}"
 
